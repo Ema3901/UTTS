b1ce584be1e006973c29359e1437cdedd9306b5b
Ema3901 <ENEma3901@gmail.com>
2025-03-31 00:14:51 -0500

    eliminar basura y agregar mejoras a splash-screen

--- www/js/splash-screen.js
+++ www/js/splash-screen.js
@@ -1,12 +1,18 @@
 // splash-screen.js
 window.onload = function() {
+    // Verificar si ya se mostró el splash screen
     if (!localStorage.getItem('splashShown')) {
         // Mostrar la splash screen durante 3 segundos
+        document.getElementById('splash-screen').style.display = 'flex'; // Asegurar que el splash screen se muestra
         setTimeout(function() {
-            document.getElementById('splash-screen').style.display = 'none';
-            localStorage.setItem('splashShown', 'true');
-        }, 3000);
+            const splashScreen = document.getElementById('splash-screen');
+            splashScreen.style.opacity = '0'; // Desvanecer el splash screen
+            localStorage.setItem('splashShown', 'true'); // Marcar que se ha mostrado
+        }, 3000); // 3 segundos para el splash screen
     } else {
+        // Si ya fue mostrado, ocultamos el splash screen inmediatamente
         document.getElementById('splash-screen').style.display = 'none';
+        // Mostrar el spinner si es necesario
+        showSpinner();
     }
 }
